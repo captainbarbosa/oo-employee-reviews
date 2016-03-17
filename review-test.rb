529b5dce143f1008c7f1623cf9ea3b6bda2d8599
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-17 15:33:37 -0400

    Creating test structure

---
+++ review-test.rb
@@ -0,0 +1,63 @@
+require 'minitest/autorun'
+require_relative 'employee'
+require_relative 'department'
+
+class EmployeeTest < Minitest::Test
+  def employee
+    ::Employee.new
+  end
+
+  # 1
+  def test_creating_a_new_department
+      skip
+  end
+
+  # 2
+  def test_creating_a_new_employee
+      skip
+  end
+
+  # 3
+  def test_adding_an_employee_to_department
+      skip
+  end
+
+  # 4
+  def test_retrieving_employee_name
+      skip
+  end
+
+  # 5
+  def test_retrieving_employee_salary
+      skip
+  end
+
+  # 6
+  def test_retrieving_department_name
+      skip
+  end
+
+  # 7
+  def test_retrieving_salary_of_all_employees_in_department
+      skip
+  end
+
+  # 8
+  def test_adding_review_text_to_employee
+      skip
+  end
+
+  # 9
+  def test_marking_employee_performance
+      skip
+  end
+
+  # 10
+  def test_give_employee_a_raise
+      skip
+  end
+
+  # 11
+  def test_give_department_a_raise
+      skip
+  end
